911fe5ce618187ec626133f946e05def3a00fe66
D5v4d <frontend2024@bk.ru>
2026-05-14 20:46:08 +0300

    fix(101): Updated routing structure

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,10 @@ import './assets/base.css'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
